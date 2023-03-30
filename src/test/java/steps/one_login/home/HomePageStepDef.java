64833a171017ea46ed5eca6e1fa5dd5bde1f5450
Azeez Oluwa <azolu@netcompany.com>
2023-03-30 10:44:09 +0100

    test data

--- src/test/java/steps/one_login/home/HomePageStepDef.java
+++ src/test/java/steps/one_login/home/HomePageStepDef.java
@@ -1,11 +1,14 @@
 package steps.one_login.home;
 
+import gov.uk.inss.pages.one_login.home.GovUKHomePage;
 import io.cucumber.java.en.Given;
+
 
 public class HomePageStepDef {
 
     @Given("I am on one login home page")
     public void i_am_on_one_login_home_page() {
+//        GovUKHomePage govUKHomePage = new GovUKHomePage();
 
     }
 }
