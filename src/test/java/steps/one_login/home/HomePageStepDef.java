a96a4bb22d4d3bb00d7a08211e554b250ab8905d
Azeez Oluwa <azolu@netcompany.com>
2023-03-31 11:16:50 +0100

    lil refac

--- src/test/java/steps/one_login/home/HomePageStepDef.java
+++ src/test/java/steps/one_login/home/HomePageStepDef.java
@@ -1,26 +1,12 @@
 package steps.one_login.home;
 
-<<<<<<< HEAD
-import gov.uk.inss.pages.one_login.home.GovUKHomePage;
-import io.cucumber.java.en.Given;
-import org.junit.Assert;
-
-
-=======
 import io.cucumber.java.en.Given;
 
->>>>>>> cf6425264699513621c73f4a89a16d2eb52837ac
 public class HomePageStepDef {
 
     @Given("I am on one login home page")
     public void i_am_on_one_login_home_page() {
-<<<<<<< HEAD
 //        GovUKHomePage govUKHomePage = new GovUKHomePage();
-<<<<<<< HEAD
-=======
-=======
->>>>>>> cf6425264699513621c73f4a89a16d2eb52837ac
 
->>>>>>> master
     }
 }
